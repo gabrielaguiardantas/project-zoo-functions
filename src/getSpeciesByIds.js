5892883946ee715776bf092ddbc1d03c442c85a9
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-10-02 15:31:45 -0300

    projeto 6, requisitos 1 e 2 ok, gustavo e vitor ajudaram demaissssss

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -9,13 +9,4 @@ function getSpeciesByIds(...ids) {
   return getSpecieById;
 }
 
-// function getSpeciesByIds(...ids) {
-//   const arrayIds = [];
-//   for (let i = 1; i < species.length; i += 1) {
-//     const saveIds = species[i].id;
-//     arrayIds.push(saveIds);
-//   }
-//   if (arrayIds.includes(...ids) return Object.) 
-// } getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d', '89be95b3-47e4-4c5b-b687-1fabf2afa274');
-
 module.exports = getSpeciesByIds;
