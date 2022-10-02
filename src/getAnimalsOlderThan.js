3919c8d9512048d6d9749b27b0fba253dcdb9057
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-10-02 15:28:16 -0300

    projeto 6, requisitos 1 e 2 ok, gustavo e vitor ajudaram demaissss

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código   aqui
+  const findSpecie = species.find((zooAnimal) => zooAnimal.name === animal).residents;
+  const allAnimalsOlderOrNot = findSpecie.every((specie) => specie.age >= age);
+  return allAnimalsOlderOrNot;
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const x = ids;
+  if (!x) return [];
+  const getSpecieById = species
+    .filter((specie) => x.some((id) => id === specie.id));
+  return getSpecieById;
 }
+
+// function getSpeciesByIds(...ids) {
+//   const arrayIds = [];
+//   for (let i = 1; i < species.length; i += 1) {
+//     const saveIds = species[i].id;
+//     arrayIds.push(saveIds);
+//   }
+//   if (arrayIds.includes(...ids) return Object.) 
+// } getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d', '89be95b3-47e4-4c5b-b687-1fabf2afa274');
 
 module.exports = getSpeciesByIds;
