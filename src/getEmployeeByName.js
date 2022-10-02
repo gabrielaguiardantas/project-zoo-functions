d80327feb37ebc8637cc5edf175a8c85f81019da
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-10-02 17:42:10 -0300

    projeto 6, requisitos 1,2,3,4 ok

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  const employee = employees.find((employe) => employe.firstName === employeeName
+  || employe.lastName === employeeName);
+  return employee;
 }
 
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const Manager = employees.some((employee) => employee.managers.includes(id));
+  return Manager;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId)) {
+    const employeesManager = employees.filter((employee) => employee.managers.includes(managerId));
+    const employeesManagerName = employeesManager.map(
+      (employee) =>
+        `${employee.firstName} ${employee.lastName}`,
+    );
+    return employeesManagerName;
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
