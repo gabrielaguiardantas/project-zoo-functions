5ea296d967b29a5707a1b58d4625f4482e0ac7dd
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-10-03 20:19:35 -0300

    projeto 6, requisitos até o 8 ok, quase terminando o 9

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,19 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const sumOfAllTickets = countEntrants(entrants).child
+   * 20.99 + countEntrants(entrants).adult * 49.99 + countEntrants(entrants).senior * 24.99;
+  return sumOfAllTickets;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,39 @@
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// função relacionada ao parâmetro da função getSchedule ser um dia da semana
+const dayParameter = (scheduleTarget) => {
+  const
+    x = `Open from ${hours[scheduleTarget].open}am until ${hours[scheduleTarget].close}pm`;
+  const y = species.filter((specie) => specie.availability.includes(scheduleTarget))
+    .map((specie) => specie.name);
+  const genericObject = (scheduleTargettt) => ({
+    [scheduleTargettt]:
+    { officeHour: x, exhibition: y } });
+  return genericObject(scheduleTarget);
+};
+
+const mondayDay = (scheduleTarget) => {
+  if (scheduleTarget === 'Monday') {
+    return {
+      Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+    };
+  } return dayParameter(scheduleTarget);
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+  // relacionada ao parâmetro ser algum animal
+  const allAnimals = species.map((specie) => specie.name);
+  if (allAnimals.includes(scheduleTarget)) {
+    return species.find((specie) => specie.name === scheduleTarget).availability;
+  }
+  // relacionado ao parâmetro ser algum dia da semana e ser monday
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  if (daysOfWeek.includes(scheduleTarget)) return mondayDay(scheduleTarget);
+
+  // relacionado ao parâmetro não condizer com dia ou animal
+  if (!daysOfWeek.includes(scheduleTarget)
+  && !allAnimals.includes(scheduleTarget)) return daysOfWeek.map((day) => dayParameter(day));
+} console.log(getSchedule('qualquercoisa'));
 
 module.exports = getSchedule;
