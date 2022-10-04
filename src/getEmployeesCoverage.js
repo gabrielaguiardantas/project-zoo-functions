9ed1676c49d2914006ddb17ec5117a08e71b5ff9
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-10-04 16:23:14 -0300

    projeto 6, requisitos até o 11 ok, seguimossss, Gustavo ajudou demais!!!

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,20 +1,55 @@
-// const { employees, species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-// const getSpeciesByIds = require('./getSpeciesByIds');
+const getSpeciesByIds = require('./getSpeciesByIds');
 
-// function getEmployeesCoverage({ name: x, id: y }) {
-//   // seu código aquiiii
-//   // if (x) {
-//   //   // const zooEmployee = employees
-//   //     .find((employee) => employee.firstName === x || employee.lastName === x);
-//   //   const genericEmployeeObject = {
-//   //     id: zooEmployee.id,
-//   //     fullName: `${zooEmployee.firstName} ${zooEmployee.lastName}`,
-//   //     species: zooEmployee.responsibleFor,
-//   //     location: 'opa',
-//   //   };
-//   //   return genericEmployeeObject;
-//   }
-// } console.log(getEmployeesCoverage({ name: 'Stephanie', id: 'aqui' }));
+// Caso o parâmetro name seja firstName ou lastName
+const getEmployeesByName = ({ name: x, id: y }) => {
+  const zooEmployee = employees
+    .find((employee) => employee.firstName === x || employee.lastName === x);
+  if (employees.some((employee) => employee.firstName === x || employee.lastName === x)) {
+    const genericEmployeeObject = {
+      id: zooEmployee.id,
+      fullName: `${zooEmployee.firstName} ${zooEmployee.lastName}`,
+      species: zooEmployee.responsibleFor.map((id) => getSpeciesByIds(id)[0].name),
+      locations: zooEmployee.responsibleFor.map((id) => getSpeciesByIds(id)[0].location),
+    };
+    return genericEmployeeObject;
+  } throw new Error('Informações inválidas');
+};
+// caso o parâmetro seja id
+const getEmployeesById = ({ name: x, id: y }) => {
+  const zooEmployee = employees.find((employee) => employee.id === y);
+  if (employees.some((employee) => employee.id === y)) {
+    const genericEmployeeObject = {
+      id: zooEmployee.id,
+      fullName: `${zooEmployee.firstName} ${zooEmployee.lastName}`,
+      species: zooEmployee.responsibleFor.map((id) => getSpeciesByIds(id)[0].name),
+      locations: zooEmployee.responsibleFor.map((id) => getSpeciesByIds(id)[0].location),
+    };
+    return genericEmployeeObject;
+  } throw new Error('Informações inválidas');
+};
+// caso não tenha parâmetros
+const getAllEmployees = () => {
+  const allZooEmployees = employees.map((employee) => ({
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: employee.responsibleFor.map((id) => getSpeciesByIds(id)[0].name),
+    locations: employee.responsibleFor.map((id) => getSpeciesByIds(id)[0].location),
+  }));
+  return allZooEmployees;
+};
 
-// module.exports = getEmployeesCoverage;
+function getEmployeesCoverage({ name: x, id: y } = {}) {
+  // relacionada ao parâmetro name ser um firstName ou lastName
+  if (x) {
+    return getEmployeesByName({ name: x, id: y });
+  }
+  // relacionada ao parâmetro id ser um id
+  if (y) {
+    return getEmployeesById({ name: x, id: y });
+  }
+  return getAllEmployees();
+}
+
+module.exports = getEmployeesCoverage;
