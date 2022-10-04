d2e18780cf7842a90f2e025b2536d964b2f7c2ac
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-10-04 13:01:28 -0300

    projeto 6, requisitos até o 10 ok, seguimos com a mente iluminada e testando erro no evaluator

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,20 @@
+// const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+// const getSpeciesByIds = require('./getSpeciesByIds');
 
-function getEmployeesCoverage({ name: x, id: y }) {
-  // seu código aquiiii
-}
+// function getEmployeesCoverage({ name: x, id: y }) {
+//   // seu código aquiiii
+//   // if (x) {
+//   //   // const zooEmployee = employees
+//   //     .find((employee) => employee.firstName === x || employee.lastName === x);
+//   //   const genericEmployeeObject = {
+//   //     id: zooEmployee.id,
+//   //     fullName: `${zooEmployee.firstName} ${zooEmployee.lastName}`,
+//   //     species: zooEmployee.responsibleFor,
+//   //     location: 'opa',
+//   //   };
+//   //   return genericEmployeeObject;
+//   }
+// } console.log(getEmployeesCoverage({ name: 'Stephanie', id: 'aqui' }));
 
-module.exports = getEmployeesCoverage;
+// module.exports = getEmployeesCoverage;
