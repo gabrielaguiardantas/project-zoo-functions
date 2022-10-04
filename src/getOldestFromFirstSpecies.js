12ecf078ce415d09ea4f72dbf9ce2892e5370ba9
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-10-04 10:24:29 -0300

    projeto 6, requisitos até o 10 ok, seguimos com a mente iluminada e VOANDO!!!

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const firstSpecie = employees.find((zooEmployee) => zooEmployee.id === id).responsibleFor[0];
+  const specieById = species.find((specie) => specie.id === firstSpecie).residents;
+  const oldestAnimal = specieById.reduce((acc, animal) => {
+    if (acc.age > animal.age) {
+      return acc;
+    } return animal;
+  });
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
